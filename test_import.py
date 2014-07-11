6340162dba95326b7a7db0ba5586f785a082954b
Andrey <sobolev.andrey@gmail.com>
2014-07-11 13:15:11 +0200

    trying some workarounds with nest module import

--- test_import.py
+++ test_import.py
@@ -1,7 +1,7 @@
 """
 >>> import imp
 >>> foo = imp.new_module("foo")
->>> foo_code = open("/home/andrey/projects/invariance/nest-build/lib/python2.7/site-packages/nest/hl_api.py", "r").read()
+>>> foo_code = open("some path", "r").read()
 >>> exec foo_code in foo.__dict__
 >>> foo.Foo.__module__
 'foo'
@@ -11,50 +11,40 @@
 import imp
 import uuid
 
-nest_package_path = "/home/andrey/projects/invariance/nest-build/lib/" \
-                "python2.7/site-packages/nest/"
-
-nest_api_path = "/home/andrey/projects/invariance/nest-build/lib/" \
-                "python2.7/site-packages/nest/hl_api.py"
-
-nest_kernel_path = "/home/andrey/projects/invariance/nest-build/lib/" \
-                "python2.7/site-packages/nest/pynestkernel.so"
+nest_init_path = "/opt/nest-dev/lib/" \
+                    "python2.7/site-packages/nest/__init__.py"
 
 
 def build_nest_module(api_module_path):
     module = imp.new_module("nest_" + uuid.uuid4().hex)
-    module_code = open(api_module_path, "rb").read()
+    module_code = open(api_module_path, "r").read()
     exec module_code in module.__dict__
-    import ipdb
-    ipdb.set_trace()
     return module
 
 
 class NetworkOne(object):
 
     def __init__(self):
-        self.nst_one = build_nest_module(nest_kernel_path)
+        self.nst_one = build_nest_module(nest_init_path)
         self.nst_one.SetKernelStatus({"overwrite_files": True})
 
 
 class NetworkTwo(object):
 
     def __init__(self):
-        self.nst_two = build_nest_module(nest_kernel_path)
+        self.nst_two = build_nest_module(nest_init_path)
         self.nst_two.SetKernelStatus({"overwrite_files": False})
 
 
 if __name__ == "__main__":
-
     import nest
     assert(nest.GetKernelStatus()["overwrite_files"] == False)
 
     n1 = NetworkOne()
-
-    import ipdb
 
     assert(nest.GetKernelStatus()["overwrite_files"] == True)
 
     n2 = NetworkTwo()
 
     assert(nest.GetKernelStatus()["overwrite_files"] == False)
+
