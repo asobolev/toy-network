ada7a03150e0fa489cfe209d504b9e0246108ca0
Andrey <sobolev.andrey@gmail.com>
2014-07-15 17:37:21 +0200

    added configurations for basic neuron types

---
+++ setup/configurations.py
@@ -0,0 +1,71 @@
+from models import *
+
+
+IAF_COND_DIFF_EXP = NeuronSetup(**{
+    'model': 'iaf_cond_diffexp_ang',
+    'para_dict': {
+        'V_th': -50.0,
+        'V_reset': -55.0,
+        't_ref': 1.0,
+        't_spike_dur': 0.5,
+        'g_L':   25.0,
+        'C_m':  500.0,
+        'E_ex':   0.0,
+        'E_in': -70.0,
+        'E_L':  -70.0,
+        'V_m':  -55.0,
+        'ratio_NMDA_AMPA': 1.0,
+        'tau_AMPA_rise': 0.5,
+        'tau_AMPA_fall': 2.4,
+        'tau_GABA_rise': 1.0,
+        'tau_GABA_fall': 7.0,
+        'tau_NMDA_rise': 5.5,
+        'tau_NMDA_fall': 100.,
+    },
+    'lat_ex_input_ports': [1],  # NMDA-AMPA input
+    'noise_paras': {  # in objsim: 0.05294 to get 3 Hz background noise
+        'p_rate': 50000.,
+        'amplitude': 0.05275
+    }
+})
+
+
+INHIBITORY_NEURON = NeuronSetup(**{
+    'model': 'iaf_cond_diffexp_ang',
+    'para_dict': {
+        'V_th': -50.0,
+        'V_reset': -55.0,
+        't_ref': 1.0,
+        't_spike_dur': 0.5,
+        'g_L':   20.0,
+        'C_m':  200.0,
+        'E_ex':   0.0,
+        'E_in': -70.0,
+        'E_L':  -70.0,
+        'V_m':  -55.0,
+        'ratio_NMDA_AMPA': 1.0,
+        'tau_AMPA_rise': 0.5,
+        'tau_AMPA_fall': 2.4,
+        'tau_GABA_rise': 1.0,
+        'tau_GABA_fall': 7.0,
+        'tau_NMDA_rise': 5.5,
+        'tau_NMDA_fall': 100.,
+        'const_GABA_input': 4.62,  # to reduce background noise to 3 Hz
+    },
+    'lat_ex_input_ports': [1],  # NMDA-AMPA input
+    'noise_paras': {  # to get 100 Hz background noise
+        'p_rate': 50000.,
+        'amplitude': 0.0504967
+    }
+})
+
+
+INPUT_NEURON = NeuronSetup(**{
+    'model': 'pixel_iaf_psc_exp',
+    'para_dict': {},
+    'lat_ex_input_ports': [],
+    'noise_paras': {  # 3 Hz background noise
+        'p_rate': 12000.,
+        'amplitude': 13.9
+    }
+})
--- setup/models.py
+++ setup/models.py
@@ -1,6 +1,30 @@
 from collections import namedtuple
 
 
+tuple_NeuronSetup = namedtuple(
+    'tuple_NeuronSetup',
+    ['model', 'para_dict', 'noise_firing_rate', 'noise_amplitude',
+     'lat_ex_input_ports']
+)
+class NeuronSetup(tuple_NeuronSetup):
+
+    @property
+    def noise_parameters(self):
+        return {
+            'p_rate': self.noise_firing_rate,
+            'amplitude': self.noise_amplitude
+        }
+
+    @property
+    def as_dict(self):
+        return {
+            'model': self.model,
+            'para_dict': self.para_dict,
+            'noise_paras': self.noise_parameters,
+            'lat_ex_input_ports': self.lat_ex_input_ports
+        }
+
+
 tuple_ImageSequenceGeneratorSetup = namedtuple(
     'tuple_ImageSequenceGeneratorSetup',
     ['movie_path', 'stimuli_duration', 'stimuli_per_object', 'nx', 'ny',
@@ -32,28 +56,4 @@ class ImageSequenceGeneratorSetup(tuple_ImageSequenceGeneratorSetup):
         return {
             'filename': self.movie_path,
             'player': self._player
-        }
-
-
-tuple_NeuronSetup = namedtuple(
-    'tuple_NeuronSetup',
-    ['model', 'para_dict', 'noise_firing_rate', 'noise_amplitude',
-     'lat_ex_input_ports']
-)
-class NeuronSetup(tuple_NeuronSetup):
-
-    @property
-    def noise_parameters(self):
-        return {
-            'p_rate': self.noise_firing_rate,
-            'amplitude': self.noise_amplitude
-        }
-
-    @property
-    def as_dict(self):
-        return {
-            'model': self.model,
-            'para_dict': self.para_dict,
-            'noise_paras': self.noise_parameters,
-            'lat_ex_input_ports': self.lat_ex_input_ports
         }
