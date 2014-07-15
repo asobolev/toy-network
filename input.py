366249504753e4913404573d9142c5f8afda004b
Andrey <sobolev.andrey@gmail.com>
2014-07-15 17:27:15 +0200

    added a setup package with models for different configurations, added input layer model draft

--- input.py
+++ input.py
@@ -1,49 +1,10 @@
+import itertools
 import nest
+import nest.topology as tp
 
 
 INPUT_MOVIE = 'data/gauss20x20.idlmov'
 
-
-class ImageSequenceGeneratorSetup(object):
-
-    def __init__(self, movie_path, stimuli_duration, stimuli_per_object, nx, ny,
-                 pre_ISI=2.5, post_ISI=2.5, type='xysequence',
-                 stim_direction='x_continuous'):
-        self.movie_path = movie_path
-        self.stimuli_duration = stimuli_duration
-        self.stimuli_per_object = stimuli_per_object
-        self.nx = nx
-        self.ny = ny
-        self.pre_ISI = pre_ISI
-        self.post_ISI = post_ISI
-        self.type = type
-        self.stim_direction = stim_direction
-
-    @property
-    def _player_parameters(self):
-        return {
-            'stimulus_interval': self.stimuli_duration,
-            'inter_stimulus_interval': self.pre_ISI,
-            'inter_stimulus_interval2': self.post_ISI,
-            'stimuli_per_object': self.stimuli_per_object,
-            'nx': self.nx,
-            'ny': self.ny,
-            'stim_direction': self.stim_direction
-        }
-
-    @property
-    def _player(self):
-        return {
-            'type': self.type,
-            'parameter': self._player_parameters
-        }
-
-    @property
-    def as_dict(self):
-        return {
-            'filename': self.movie_path,
-            'player': self._player
-        }
 
 
 def image_sequence_generator_showcase():
@@ -71,6 +32,64 @@ def image_sequence_generator_showcase():
     for i in range(stimuli_per_object):
         frame_num = nest.GetStatus(img_seq, 'current_frame_num')[0]
         nest.Simulate(single_stim_time)
+
+
+
+class InputLayer(object):
+
+    def __init__(self, input_weight, ISG_setup, neuron_setup, x_dim=5, y_dim=5):
+        """
+        Constructor of the input layer. An input layer is a flat layer with
+        neurons that fire according to the intensity of the presented images as
+         sequence in time. Images are parsed from the given movie file inside
+         ImageSequenceGeneratorSetup settings.
+
+        :param input_weight     weight for the connections between the image 
+                                generator and the neurons
+        :param ISG_setup:       ImageSequenceGeneratorSetup object with image
+                                parsing settings
+        :param neuron_setup:    NeuronSetup object with settings for the
+                                neurons for the input layer
+        """
+        self._nest_layer = tp.CreateLayer({
+            'rows': x_dim,
+            'columns': y_dim,
+            'elements': neuron_setup.model,
+            'edge_wrap': True
+        })
+
+        self._nest_nodes = nest.GetNodes(self._nest_layer)[0]
+        nest.SetStatus(self._nest_nodes, {"V_m":0.})
+
+        self._movie = nest.Create('image_sequence_generator', 1, ISG_setup.as_dict)
+        
+        nodes = iter(self._nest_nodes) 
+        for x, y in itertools.product(range(x_dim), range(y_dim)):
+            node = nodes.next()
+            nest.SetStatus([node], {'x': x, 'y': y, 'weight': input_weight})
+            nest.Connect(self._movie, [node])
+
+        self._spikes = nest.Create('spike_detector', 1, [{'label': 'input_spikes'}])
+        nest.ConvergentConnect(self._nest_nodes, self._spikes)
+
+    @property
+    def layer(self):
+        return self._nest_layer
+
+    @property
+    def nodes(self):
+        return self._nest_nodes
+
+    @property
+    def spikes(self):
+        return self._spikes
+
+    @property
+    def movie(self):
+        return self._movie
+
+    def clear_spike_detector(self):
+        nest.SetStatus(self._spikes, 'n_events', 0)
 
 
 if __name__=='__main__':
---
+++ setup/models.py
@@ -0,0 +1,59 @@
+from collections import namedtuple
+
+
+tuple_ImageSequenceGeneratorSetup = namedtuple(
+    'tuple_ImageSequenceGeneratorSetup',
+    ['movie_path', 'stimuli_duration', 'stimuli_per_object', 'nx', 'ny',
+     'pre_ISI', 'post_ISI', 'type', 'stim_direction']
+)
+class ImageSequenceGeneratorSetup(tuple_ImageSequenceGeneratorSetup):
+
+    @property
+    def _player_parameters(self):
+        return {
+            'stimulus_interval': self.stimuli_duration,
+            'inter_stimulus_interval': self.pre_ISI,
+            'inter_stimulus_interval2': self.post_ISI,
+            'stimuli_per_object': self.stimuli_per_object,
+            'nx': self.nx,
+            'ny': self.ny,
+            'stim_direction': self.stim_direction
+        }
+
+    @property
+    def _player(self):
+        return {
+            'type': self.type,
+            'parameter': self._player_parameters
+        }
+
+    @property
+    def as_dict(self):
+        return {
+            'filename': self.movie_path,
+            'player': self._player
+        }
+
+
+tuple_NeuronSetup = namedtuple(
+    'tuple_NeuronSetup',
+    ['model', 'para_dict', 'noise_firing_rate', 'noise_amplitude',
+     'lat_ex_input_ports']
+)
+class NeuronSetup(tuple_NeuronSetup):
+
+    @property
+    def noise_parameters(self):
+        return {
+            'p_rate': self.noise_firing_rate,
+            'amplitude': self.noise_amplitude
+        }
+
+    @property
+    def as_dict(self):
+        return {
+            'model': self.model,
+            'para_dict': self.para_dict,
+            'noise_paras': self.noise_parameters,
+            'lat_ex_input_ports': self.lat_ex_input_ports
+        }
