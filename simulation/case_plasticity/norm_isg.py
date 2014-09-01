7dfc5f5c4b717a2df94e190a39dd0f360a0dd522
asobolev <sobolev.andrey@gmail.com>
2014-09-01 13:59:46 +0200

    found suitable configuration for 9x2 neurons

--- simulation/case_plasticity/norm_isg.py
+++ simulation/case_plasticity/norm_isg.py
@@ -21,7 +21,7 @@ import matplotlib.pyplot as plt
 
 from reduced.setup.inputs import ISGStraightSetup
 from reduced.setup.neurons import NeuronSetup
-from reduced.network.layer import InputLayer
+from reduced.network.layer import InputLayer, MapLayer
 from reduced.simulation.plot.dynamics import *
 
 
@@ -30,8 +30,8 @@ def execute(conn_weight):
 
     # 5x5 input layer
     input_setup = ISGStraightSetup(**{
-        "stimuli_duration": 200.0,
-        "i_s_i": 50.0,
+        "stimuli_duration": 15.0,
+        "i_s_i": 85.0,
         "movie_path": "../../data/5x5gklearn0.idlmov"
     })
     neuron_setup = NeuronSetup(**{
@@ -55,27 +55,30 @@ def execute(conn_weight):
             "V_reset": -55.0
         }
     })
-    output_id = nest.Create(neuron_setup.model, params=neuron_setup.para_dict)[0]
+    output_1 = nest.Create(neuron_setup.model, params=neuron_setup.para_dict)[0]
+    output_2 = nest.Create(neuron_setup.model, params=neuron_setup.para_dict)[0]
+    #map_layer = MapLayer(neuron_setup, , x_dim=3, y_dim=3)
 
     inputs = input_layer.nodes
-    outputs = [output_id]
+    outputs = [output_1, output_2]
 
     # plastic connection
     synapse_setup = {
         'alpha': 0.1,
         'lambda': 0.01,
         'weight': 0.05,
-        'norm_freq': 400.,
-        'norm_fac1': 0.9,
+        'norm_freq': 800.,
+        'norm_fac1': 0.7,
         'norm_fac0': 0.2
     }
     nest.CopyModel('stdp_pl_norm_synapse_hom', 'plastic', synapse_setup)
 
-    weights = 20.0 * np.random.rand(len(inputs))
-
     syn_spec = lambda x: {'weight': x, 'model': 'plastic'}
-    for input_id, weight in zip(inputs, weights):
-        nest.Connect([input_id], outputs, syn_spec=syn_spec(weight))
+    for input_id in inputs:
+        weight = 200.0 * np.random.random()
+        nest.Connect([input_id], [outputs[0]], syn_spec=syn_spec(weight))
+        weight = 200.0 * np.random.random()
+        nest.Connect([input_id], [outputs[1]], syn_spec=syn_spec(weight))
 
     connections = nest.GetConnections(inputs, outputs)
 
@@ -89,13 +92,18 @@ def execute(conn_weight):
 
     # simulation
     scales = []
-    for t in range(50):
+    phase = 3 * input_setup.i_s_i + input_setup.stimuli_duration
+    for t in range(int(100000/phase)):
         state = [nest.GetStatus([synapse_id])[0] for synapse_id in connections]
         scales.append(state)
-        nest.Simulate(1000.0)
+        nest.Simulate(phase)
 
     # results
+    #import ipdb
+    #ipdb.set_trace()
+
     print [x['weight'] for x in scales[0]]
+    print [x['weight'] for x in scales[1]]
     print [x['weight'] for x in scales[-1]]
 
     output = []
